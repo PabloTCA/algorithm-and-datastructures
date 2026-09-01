2ad3847ef5416460808f00cbc4ffcda80fe0e8df
Pablo Chavez <paburochavezu@gmail.com>
2026-08-31 21:51:40 -0700

    MinHeap Class with the same methods but to work with the minHeap logic

--- Heaps/HeapClass.cpp
+++ Heaps/HeapClass.cpp
@@ -100,25 +100,100 @@ public:
         }
     }
 };
+class MinHeap
+{
+private:
+    std::vector<int> heap;
+    int leftChild(int index)
+    {
+        return 2 * index + 1;
+    }
+    int rightChild(int index)
+    {
+        return 2 * index + 2;
+    }
+    int parent(int index)
+    {
+        return (index - 1) / 2;
+    }
+    void swap(int index1, int index2)
+    {
+        int temp = heap[index1];
+        heap[index1] = heap[index2];
+        heap[index2] = temp;
+    }
+public:
+    void printHeap()
+    {
+        std::cout << "\n[";
+        for (size_t i = 0; i < heap.size(); i++)
+        {
+            std::cout << heap[i];
+            if (i < heap.size())
+            {
+                std::cout << ", ";
+            }
+        }
+        std::cout << "]" << std::endl;
+    }
+    void insert(int value)
+    {
+        heap.push_back(value);
+        int current = heap.size() - 1;
+        while (current > 0 && heap[current] < heap[parent(current)])
+        {
+            swap(current, parent(current));
+            current = parent(current);
+        }
+    }
+    int remove()
+    {
+        if (heap.size() == 0) return INT_MIN;
+        int minValue = heap.front();
+        if (heap.size() == 1)
+        {
+            heap.pop_back();
+        } else
+        {
+            heap[0] = heap.back();
+            heap.pop_back();
+            sinkDown(0);
+        }
+        return minValue;
+    }
+};
 
 
 int main()
 {
-    Heap* myHeap = new Heap();
-    myHeap->insert(99);
-    myHeap->insert(72);
-    myHeap->insert(61);
-    myHeap->insert(58);
+    MinHeap* min_heap = new MinHeap();
+    min_heap->insert(99);
+    min_heap->insert(80);
+    min_heap->insert(60);
+    min_heap->insert(50);
+    min_heap->insert(25);
+    min_heap->insert(10);
+    min_heap->insert(2);
+    min_heap->insert(1);
 
-    myHeap->printHeap();
+    min_heap->printHeap();
 
-    myHeap->insert(100);
-    myHeap->printHeap();
 
-    myHeap->insert(75);
-    myHeap->printHeap();
-
-    myHeap->remove();
-    myHeap->printHeap();
+    // Heap* myHeap = new Heap();
+    // myHeap->insert(99);
+    // myHeap->insert(72);
+    // myHeap->insert(61);
+    // myHeap->insert(58);
+    //
+    // myHeap->printHeap();
+    //
+    // myHeap->insert(100);
+    // myHeap->printHeap();
+    //
+    // myHeap->insert(75);
+    // myHeap->printHeap();
+    //
+    // myHeap->remove();
+    // myHeap->printHeap();
 
 }
